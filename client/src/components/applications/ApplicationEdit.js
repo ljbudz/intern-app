49c37cfd5de9e5b63fa971e40f911ee119c19970
Lucas Budz <lucasjbudz@gmail.com>
2020-03-21 19:53:59 -0400

    Configure show page and edit form

--- client/src/components/applications/ApplicationEdit.js
+++ client/src/components/applications/ApplicationEdit.js
@@ -1,9 +1,43 @@
+import _ from "lodash";
 import React from "react";
+import { connect } from "react-redux";
+import { fetchApplication, editApplication } from "../../actions";
+import ApplicationForm from "./ApplicationForm";
+import { Loader } from "semantic-ui-react";
 
 class ApplicationEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchApplication(this.props.match.params.id);
+  }
+
+  onSubmit = formValues => {
+    this.props.editApplication(this.props.match.params.id, formValues);
+  };
+
   render() {
-    return <div>Edit</div>;
+    if (!this.props.application) {
+      return (
+        <Loader active size="medium">
+          Loading...
+        </Loader>
+      );
+    }
+    return (
+      <div>
+        <h1>Edit Application</h1>
+        <ApplicationForm
+          onSubmit={this.onSubmit}
+          initialValues={_.pick(this.props.application, "title", "company")}
+        />
+      </div>
+    );
   }
 }
 
-export default ApplicationEdit;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    application: state.applications[ownProps.match.params.id]
+  };
+};
+
+export default connect(mapStateToProps, { fetchApplication, editApplication })(ApplicationEdit);
--- client/src/components/applications/ApplicationShow.js
+++ client/src/components/applications/ApplicationShow.js
@@ -1,9 +1,49 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchApplication } from "../../actions";
+import { Link } from "react-router-dom";
+import { Item, Loader, Button } from "semantic-ui-react";
 
 class ApplicationShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchApplication(this.props.match.params.id);
+  }
+
   render() {
-    return <div>Show</div>;
+    if (!this.props.application) {
+      return (
+        <Loader active size="medium">
+          Loading...
+        </Loader>
+      );
+    }
+
+    const { title, company, id } = this.props.application;
+
+    return (
+      <Item.Group>
+        <Item>
+          <Item.Image size="tiny" src={`//logo.clearbit.com/${company}.com`} />
+          <Item.Content>
+            <Item.Header as="h2">{title}</Item.Header>
+            <Item.Meta>{company}</Item.Meta>
+          </Item.Content>
+        </Item>
+        <Item.Extra>
+          <Button primary as={Link} to={`/applications/edit/${id}`}>
+            Edit
+          </Button>
+          <Button negative as={Link} to={`/applications/delete/${id}`}>
+            Delete
+          </Button>
+        </Item.Extra>
+      </Item.Group>
+    );
   }
 }
 
-export default ApplicationShow;
+const mapStateToProps = (state, ownProps) => {
+  return { application: state.applications[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchApplication })(ApplicationShow);
